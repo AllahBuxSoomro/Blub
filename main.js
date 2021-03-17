afea11794ff6ed20b40877e4e9cb10160f6dc413
Allah Bux Soomro <78809560+AllahBuxSoomro@users.noreply.github.com>
2021-03-17 23:33:45 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function onBlub() {
+  document.querySelector("img").src = "./img/bulbon.png";
+}
+function offBlub() {
+  document.querySelector("img").src = "./img/bulboff.png";
+}
+
+/*function sum(num1, num2) {
+  console.log(num1 + num2);
+  return;
+}
+sum(50, 48);*/
